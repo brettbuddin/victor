4e9fe79f090bf2b461406266d57a2ededdf1ad98
Brett Buddin <brett@intraspirit.net>
2012-08-12 19:19:41 -0400

    Exposing getters for Client and Brain.
    This will allow you to send messages outside of a Matcher context.

--- campfire.go
+++ campfire.go
@@ -25,13 +25,29 @@ func NewCampfire(name string, account string, token string, rooms []int) *Campfi
     }
 }
 
+func (self *Campfire) Brain() *Brain {
+    return self.brain
+}
+
+func (self *Campfire) Client() *campfire.Client {
+    return self.client
+}
+
+func (self *Campfire) Hear(expStr string, callback func(*TextMessage)) {
+    self.Brain().Hear(expStr, callback)
+}
+
+func (self *Campfire) Respond(expStr string, callback func(*TextMessage)) {
+    self.Brain().Respond(expStr, callback)
+}
+
 func (self *Campfire) Run() {
     rooms := self.rooms
     messages := make(chan *campfire.Message)
     joined := 0
 
     for i := range rooms {
-        me, err := self.client.Me()
+        me, err := self.Client().Me()
 
         if err != nil {
             log.Printf("Error fetching info about self: %s", err)
@@ -40,7 +56,7 @@ func (self *Campfire) Run() {
 
         self.me = me
 
-        room := self.client.Room(rooms[i])
+        room := self.Client().Room(rooms[i])
 
         if room.Join() != nil {
             log.Printf("Error joining room %i: %s", rooms[i], err)
@@ -69,27 +85,19 @@ func (self *Campfire) Run() {
 
                 Reply: self.reply(in.RoomId, in.UserId),
                 Send: func(text string) {
-                    self.client.Room(in.RoomId).Say(text)
+                    self.Client().Room(in.RoomId).Say(text)
                 },
                 Paste: func(text string) {
-                    self.client.Room(in.RoomId).Paste(text)
+                    self.Client().Room(in.RoomId).Paste(text)
                 },
                 Sound: func(name string) {
-                    self.client.Room(in.RoomId).Sound(name)
+                    self.Client().Room(in.RoomId).Sound(name)
                 },
             }
 
             go self.brain.Receive(msg)
         }
     }
-}
-
-func (self *Campfire) Hear(expStr string, callback func(*TextMessage)) {
-    self.brain.Hear(expStr, callback)
-}
-
-func (self *Campfire) Respond(expStr string, callback func(*TextMessage)) {
-    self.brain.Respond(expStr, callback)
 }
 
 func (self *Campfire) pollRoomDetails(room *campfire.Room) {
@@ -101,7 +109,7 @@ func (self *Campfire) pollRoomDetails(room *campfire.Room) {
         }
 
         for _, user := range details.Users {
-            self.brain.RememberUser(&User{Id: user.Id, Name: user.Name})
+            self.Brain().RememberUser(&User{Id: user.Id, Name: user.Name})
         }
 
         time.Sleep(300 * time.Second)
@@ -109,8 +117,8 @@ func (self *Campfire) pollRoomDetails(room *campfire.Room) {
 }
 
 func (self *Campfire) reply(roomId int, userId int) func(string) {
-    room := self.client.Room(roomId)
-    user := self.brain.UserForId(userId)
+    room := self.Client().Room(roomId)
+    user := self.Brain().UserForId(userId)
     prefix := ""
 
     if user != nil {
