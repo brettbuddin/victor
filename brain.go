e1b4f93d9e314324eb35715834d063a1adf133b0
Brett Buddin <brett@intraspirit.net>
2012-08-12 14:33:45 -0400

    Moved these bits out into their own file

--- brain.go
+++ brain.go
@@ -4,8 +4,6 @@ import (
     "log"
     "regexp"
     "strings"
-
-    "github.com/brettbuddin/victor/utils/google"
 )
 
 type RobotAdapter interface {
@@ -27,25 +25,28 @@ func NewBrain(name string) *Brain {
         matchers: make([]*Matcher, 0, 1),
     }
 
-    brain.registerDefaultAbilities()
+    RegisterDefaultAbilities(brain)
 
     return brain
 }
 
+func (self *Brain) AddMatcher(m *Matcher) {
+    self.matchers = append(self.matchers, m)
+
+    log.Printf("Pattern: /%s/", m.Pattern.String())
+}
+
 func (self *Brain) Hear(expStr string, callback func(*TextMessage)) {
     exp, _ := regexp.Compile(expStr)
-    self.matchers = append(self.matchers, NewMatcher(exp, callback))
 
-    log.Printf("Pattern: /%s/", exp.String())
+    self.AddMatcher(NewMatcher(exp, callback))
 }
 
 func (self *Brain) Respond(expStr string, callback func(*TextMessage)) {
     expWithNameStr := "^(" + self.name + "[:,]?)\\s*(?:" + expStr + ")"
     exp, _ := regexp.Compile(strings.ToLower(expWithNameStr))
 
-    self.matchers = append(self.matchers, NewMatcher(exp, callback))
-
-    log.Printf("Pattern: /%s/", exp.String())
+    self.AddMatcher(NewMatcher(exp, callback))
 }
 
 func (self *Brain) Receive(msg *TextMessage) {
@@ -71,6 +72,10 @@ func (self *Brain) RememberUser(user *User) {
     self.users = append(self.users, user)
 }
 
+func (self *Brain) Users() []*User {
+    return self.users
+}
+
 func (self *Brain) UserForId(id int) *User {
     for _, user := range self.users {
         if user.Id == id {
@@ -83,36 +88,4 @@ func (self *Brain) UserForId(id int) *User {
 
 func (self *Brain) Shutdown() {
     log.Print("See ya!")
-}
-
-func (self *Brain) registerDefaultAbilities() {
-    self.Respond("ping", func(msg *TextMessage) {
-        msg.Send("Pong!")
-    })
-
-    self.Respond("(image|img) (.*)", func(msg *TextMessage) {
-        result, err := google.ImageSearch(msg.Matches()[3])
-
-        if err != nil {
-            log.Print(err)
-            return
-        }
-
-        if result == "" {
-            msg.Send("I didn't find anything.")
-            return
-        }
-
-        msg.Send(result)
-    })
-
-    self.Respond("(list|show) users", func(msg *TextMessage) {
-        list := ""
-
-        for _, user := range self.users {
-            list += user.Name + "\n"
-        }
-
-        msg.Paste(list)
-    })
 }
---
+++ defaults.go
@@ -0,0 +1,36 @@
+package victor
+
+import (
+    "github.com/brettbuddin/victor/utils/google"
+)
+
+func RegisterDefaultAbilities(brain *Brain) {
+    brain.Respond("ping", func(msg *TextMessage) {
+        msg.Send("Pong!")
+    })
+
+    brain.Respond("(image|img) (.*)", func(msg *TextMessage) {
+        result, err := google.ImageSearch(msg.Matches()[3])
+
+        if err != nil {
+            return
+        }
+
+        if result == "" {
+            msg.Send("I didn't find anything.")
+            return
+        }
+
+        msg.Send(result)
+    })
+
+    brain.Respond("(list|show) users", func(msg *TextMessage) {
+        list := ""
+
+        for _, user := range brain.Users() {
+            list += user.Name + "\n"
+        }
+
+        msg.Paste(list)
+    })
+}
