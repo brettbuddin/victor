0bfc317a3097c7113f6334f594b58aeac1a80e14
Brett Buddin <brett@intraspirit.net>
2012-08-12 14:26:25 -0400

    Pattern instead of Exp.

--- matcher.go
+++ matcher.go
@@ -6,19 +6,19 @@ import (
 )
 
 type Matcher struct {
-    Exp      *regexp.Regexp
+    Pattern  *regexp.Regexp
     Callback func(*TextMessage)
 }
 
 func NewMatcher(exp *regexp.Regexp, callback func(*TextMessage)) *Matcher {
     return &Matcher{
-        Exp:      exp,
+        Pattern:  exp,
         Callback: callback,
     }
 }
 
 func (self *Matcher) Test(msg *TextMessage) bool {
-    results := self.Exp.FindAllStringSubmatch(strings.ToLower(msg.Body), -1)
+    results := self.Pattern.FindAllStringSubmatch(strings.ToLower(msg.Body), -1)
 
     if len(results) > 0 {
         msg.SetMatches(results[0])
