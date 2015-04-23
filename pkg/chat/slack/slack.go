542f4751f024e75e6296aa6b088a488d32c20cab
Paul Seiffert <paul.seiffert@jimdo.com>
2015-04-23 17:42:04 +0200

    Adapted to new Incoming Webhooks API

--- pkg/chat/slack/slack.go
+++ pkg/chat/slack/slack.go
@@ -2,40 +2,41 @@ package slack
 
 import (
 	"encoding/json"
-	"fmt"
-	"github.com/brettbuddin/victor/pkg/chat"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+
+	"github.com/brettbuddin/victor/pkg/chat"
 )
 
 func init() {
 	chat.Register("slack", func(r chat.Robot) chat.Adapter {
-		team := os.Getenv("VICTOR_SLACK_TEAM")
-		token := os.Getenv("VICTOR_SLACK_TOKEN")
+		slackOutgoingWebhookPath := os.Getenv("SLACK_OUTGOING_WEBHOOK")
+		slackIncomingWebhookUri := os.Getenv("SLACK_INCOMING_WEBHOOK_URI")
 
-		if team == "" || token == "" {
-			log.Println("The following environment variables are required:")
-			log.Println("VICTOR_SLACK_TEAM, VICTOR_SLACK_TOKEN")
+		if slackOutgoingWebhookPath == "" || slackIncomingWebhookUri == "" {
+			log.Println("The following environment variable is required:")
+			log.Println("SLACK_INCOMING_WEBHOOK_URI, SLACK_OUTGOING_WEBHOOK")
 			os.Exit(1)
 		}
 
 		return &slack{
-			robot: r,
-			team:  team,
-			token: token,
+			robot:               r,
+			outgoingWebhookPath: slackOutgoingWebhookPath,
+			incomingWebhookUri:  slackIncomingWebhookUri,
 		}
 	})
 }
 
 type slack struct {
-	robot       chat.Robot
-	team, token string
+	robot               chat.Robot
+	incomingWebhookUri  string
+	outgoingWebhookPath string
 }
 
 func (s *slack) Run() {
-	s.robot.HTTP().HandleFunc("/hubot/slack-webhook", func(w http.ResponseWriter, r *http.Request) {
+	s.robot.HTTP().HandleFunc(s.outgoingWebhookPath, func(w http.ResponseWriter, r *http.Request) {
 		s.robot.Receive(&message{
 			userID:      r.PostFormValue("user_id"),
 			userName:    r.PostFormValue("user_name"),
@@ -57,8 +58,11 @@ func (s *slack) Send(channelID, msg string) {
 		log.Println("error sending to chat:", err)
 	}
 
-	endpoint := fmt.Sprintf("https://%s.slack.com/services/hooks/incoming-webhook?token=%s", s.team, s.token)
-	http.PostForm(endpoint, url.Values{"payload": {string(body)}})
+	if resp, err := HttpClient.PostForm(s.incomingWebhookUri, url.Values{"payload": {string(body)}}); err != nil {
+		log.Printf("Slack API call failed: %s", err)
+	} else if resp.StatusCode != 200 {
+		log.Printf("Slack API returned HTTP status code %d", resp.StatusCode)
+	}
 }
 
 func (s *slack) Stop() {
