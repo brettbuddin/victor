134d361ddc20a5fab793556cde73045932b9c7ff
Michael Stewart <michaels@fogcreek.com>
2015-06-10 13:58:10 -0400

    Altered dispatch to use an array (slice) of handlers rather than a map.
    This makes handler execution order consistent with the order of handler
    registration and allows for a default "match all unmatched commands"
    handler which is only executed if no other handlers have matched.
    
    This should not decrease the chatbot framework's efficiency since the
    previous map implementation performed a linear loop through its handlers
    regardless, making an array container more suited for the task.
    
    The future performance implications that I could think of would be if a
    "delete handler" operation was added although this could also perform a
    linear search through what will hopefully be a limited number of
    handlers.

--- dispatch.go
+++ dispatch.go
@@ -1,20 +1,39 @@
 package victor
 
 import (
-	"github.com/brettbuddin/victor/pkg/chat"
 	"regexp"
 	"strings"
+
+	"github.com/brettbuddin/victor/pkg/chat"
 )
+
+type HandlerPair interface {
+	Exp() *regexp.Regexp
+	Handler() Handler
+}
+
+type handlerPair struct {
+	exp    *regexp.Regexp
+	handle Handler
+}
+
+func (pair *handlerPair) Exp() *regexp.Regexp {
+	return pair.exp
+}
+
+func (pair *handlerPair) Handler() Handler {
+	return pair.handle
+}
 
 type dispatch struct {
 	robot    Robot
-	handlers map[*regexp.Regexp]Handler
+	handlers []HandlerPair
 }
 
 func newDispatch(bot Robot) *dispatch {
 	return &dispatch{
 		robot:    bot,
-		handlers: make(map[*regexp.Regexp]Handler),
+		handlers: make([]HandlerPair, 0, 10),
 	}
 }
 
@@ -39,7 +58,10 @@ func (d *dispatch) HandleFunc(exp string, f HandlerFunc) {
 }
 
 func (d *dispatch) handle(exp string, h Handler) {
-	d.handlers[regexp.MustCompile(exp)] = h
+	d.handlers = append(d.handlers, &handlerPair{
+		exp:    regexp.MustCompile(exp),
+		handle: h,
+	})
 }
 
 // Direct wraps a regexp pattern in the necessary pattern
@@ -56,12 +78,12 @@ func (d *dispatch) Direct(exp string) string {
 
 // ProcessMessage finds a match for a message and runs its Handler
 func (d *dispatch) ProcessMessage(m chat.Message) {
-	for exp, handler := range d.handlers {
-		matches := exp.FindAllStringSubmatch(m.Text(), -1)
+	for _, pair := range d.handlers {
+		matches := pair.Exp().FindAllStringSubmatch(m.Text(), -1)
 
 		if len(matches) > 0 {
 			params := matches[0][1:]
-			handler.Handle(&state{
+			pair.Handler().Handle(&state{
 				robot:   d.robot,
 				message: m,
 				params:  params,
